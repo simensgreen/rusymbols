dd90c7f3b108d27d49f9e94e3b98643ddc10613c
simensgreen <simensgreen007@gmail.com>
2021-03-01 09:31:24 +0300

    cleanup

--- src/core.rs
+++ src/core.rs
@@ -1,11 +1,2 @@
 //! Module with basic logic, expressions, actions
 
-mod actions;
-mod expression;
-mod brackets;
-pub mod literals;
-
-
-pub use crate::core::expression::Expression;
-pub use crate::core::actions::Actions;
-pub use crate::core::brackets::Brackets;
--- src/lib.rs
+++ src/lib.rs
@@ -21,52 +21,15 @@
 //!     Just use it! The main idea is simplicity. Enjoy!
 //! ## Example
 //! ```
-//! use rusymbols::Expression;
-//! use std::collections::HashMap;
-//!
-//! let x = Expression::new_var("x");
-//! let two = Expression::new_val(2.0);
-//! let polynomial = x.clone().pow(two.clone()) - x + two;
-//!
-//! let mut args: HashMap<&str, f64> = HashMap::new();
-//! args.insert("x", 1.0);
-//!
-//! assert_eq!(polynomial.to_string(), "x**2 - x + 2");
-//! assert_eq!(polynomial.eval_args(&args).unwrap(), 2.0);
 //! ```
 
 pub mod core;
-pub use crate::core::Expression;
 
 #[cfg(test)]
 mod tests {
     use crate::core;
-
     #[test]
-    fn expressions_priority_eq() {
-        assert_eq!(core::Actions::Add, core::Actions::Add);
-        assert_eq!(core::Actions::Mul, core::Actions::Mul);
-        assert_eq!(core::Actions::Div, core::Actions::Div);
-        assert_eq!(core::Actions::Pow, core::Actions::Pow);
-        assert_eq!(core::Actions::Brackets(Box::from(core::Expression::default()),
-                                           core::Brackets::Round),
-                   core::Actions::Brackets(Box::from(core::Expression::default()),
-                                           core::Brackets::Round));
-        assert_eq!(core::Actions::Var(String::from("x")), core::Actions::Var(String::from("x")));
-        assert_eq!(core::Actions::Val(0.0), core::Actions::Val(1.0));
-        assert_eq!(core::Actions::Val(2.0), core::Actions::Var(String::from("x")));
-        assert_eq!(core::Actions::Var(String::from("x")), core::Actions::Val(3.0));
-        assert_ne!(core::Actions::Mul, core::Actions::Add);
-        assert_ne!(core::Actions::Add, core::Actions::Mul);
-        assert_ne!(core::Actions::Add, core::Actions::Pow);
-        assert_ne!(core::Actions::Mul, core::Actions::Pow);
-    }
+    fn foo() {
 
-    #[test]
-    fn expressions_priority_ord() {
-        assert!(core::Actions::Add < core::Actions::Mul);
-        assert!(core::Actions::Mul < core::Actions::Pow);
-        assert!(core::Actions::Pow < core::Actions::Brackets(
-            Box::new(core::Expression::default()), core::Brackets::Round))
     }
 }
